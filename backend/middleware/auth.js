ece94911421b889ada3011ab62a219757c9e04bc
laoz <laozofficial@gmail.com>
2023-01-28 07:56:36 +0100

    jwt secret update

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,37 +3,37 @@ const asyncHandler = require('express-async-handler');
 const User = require('../models/userModel');
 
 
-const authGuard = asyncHandler(async(req, res, next) => {
+const authGuard = asyncHandler(async (req, res, next) => {
 
 
-    let token;
+  let token;
 
-    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-      try {
-        // Get token from header
-        token = req.headers.authorization.split(' ')[1]
-        // Verify token
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        // Get user from the token
-        req.user = await User.findById(decoded.id).select('-password')
-        next()
-      } catch (error) {
-            console.log(error);
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    try {
+      // Get token from header
+      token = req.headers.authorization.split(' ')[1]
+      // Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET || 'cacant@secret')
+      // Get user from the token
+      req.user = await User.findById(decoded.id).select('-password')
+      next()
+    } catch (error) {
+      console.log(error);
 
-            const response = {
-                'error': 'please login to continue'
-            };
+      const response = {
+        'error': 'please login to continue'
+      };
 
-            res.status(401).json(response);
-      }
+      res.status(401).json(response);
     }
-  
-    if (!token) {
-        const response = {
-            'error': 'No authorization token'
-        }
-        res.status(401).json(response);
+  }
+
+  if (!token) {
+    const response = {
+      'error': 'No authorization token'
     }
+    res.status(401).json(response);
+  }
 
 
 });
