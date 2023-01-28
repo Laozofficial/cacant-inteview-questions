ee7a3e752ff01e37177e9beeea9bed080998ccc6
ifeoluwa alao ojo <laozofficial@gmail.com>
2023-01-28 18:45:36 +0100

    added the mongodb link directly

--- backend/config/db.js
+++ backend/config/db.js
@@ -10,7 +10,7 @@ const connectDb = async () => {
         MONGO_URI
     } = process.env;
     // let uri = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`
-    let uri = `${MONGO_URI}`
+    let uri = 'mongodb+srv://laozofficial:L!M123456@cacant.ipbw750.mongodb.net/?retryWrites=true&w=majority'
     try {
         const conn = await mongoose.connect(uri)
         console.log(`connected to ${conn.connection.host}`);
